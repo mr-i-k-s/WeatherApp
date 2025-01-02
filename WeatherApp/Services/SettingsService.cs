f203dabf7b78ed35aa8b56cc3d15a0ca3de50c4a
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-02 22:02:27 +0800

    Add class SettingsService with simple Interface implementation

---
+++ WeatherApp/Services/SettingsService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Interfaces;
+
+namespace WeatherApp.Services
+{
+	public class SettingsService : ISettingsService
+	{
+		public bool Delete(string key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string Get(string key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Set(string key, string value)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
