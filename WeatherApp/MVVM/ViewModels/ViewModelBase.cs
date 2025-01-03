f254b4d63b797a8aaf1ef74e1b054d6e83ec25b6
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-03 10:28:53 +0800

    Add class ViewModelBase and Implement Interfaces

---
+++ WeatherApp/MVVM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WeatherApp.Interfaces;
+
+namespace WeatherApp.MVVM.ViewModels
+{
+	public class ViewModelBase : ObservableValidator, IViewAware, IDisposable
+	{
+		public Window View { get; set; }
+
+		public void Close()
+		{
+			View.Close();
+		}
+
+		public void Close(bool? result)
+		{
+			View.DialogResult = result;
+			View.Close();
+		}
+
+		public virtual void Dispose()
+		{
+
+		}
+	}
+}
