73ec3b90376c32a9c0337c49d0806b80d99bd798
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-03 09:59:58 +0800

    Implement FindWindowInAssebly() of class WindowService

--- WeatherApp/Services/WindowService.cs
+++ WeatherApp/Services/WindowService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,6 +27,36 @@ namespace WeatherApp.Services
 			var view = SetUpWindow(window, isDialog);
 
 			return view;
+		}
+
+		private Window FindWindowInAssebly(object viewModel)
+		{
+			Type viewModelType = viewModel.GetType();
+			string viewName = viewModelType.Name.Replace("ViewModel", "View");
+			Type? viewType = Assembly.GetEntryAssembly().GetTypes().FirstOrDefault(x => x.Name.Equals(viewName));
+
+			if (viewType != null)
+			{
+				//var view = Activator.CreateInstance(viewType) as Window;
+				var view = App.ServiceProvider.GetRequiredService(viewType) as Window;
+
+				if (view != null)
+				{
+					view.DataContext = viewModel;
+
+					var viewAware = viewModel as IViewAware;
+					if (viewAware != null)
+					{
+						viewAware.View = view;
+					}
+
+					return view;
+				}
+				else { throw new Exception("View is not of type Window"); }
+
+			}
+			else { throw new Exception("View of Viewmodel in Assebly not found"); }
+
 		}
 	}
 }
