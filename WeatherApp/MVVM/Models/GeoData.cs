45631d8f5bba2f23f52a9813ffa4da051813eab3
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-03 10:14:36 +0800

    Add class GeoData and Implement Interface IGeoData

---
+++ WeatherApp/MVVM/Models/GeoData.cs
@@ -0,0 +1,11 @@
+﻿using WeatherApp.Interfaces;
+
+namespace WeatherApp.MVVM.Models
+{
+	public class GeoData : IGeoData
+	{
+		public string Name { get; set; }
+		public string Country { get; set; }
+		public string State { get; set; }
+	}
+}
