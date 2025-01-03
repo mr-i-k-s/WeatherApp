f0f9988e6fa88883c8a2d0aa4022b5da65ebd69e
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-03 09:56:58 +0800

    Add class WindowService and Implement simple IWindowService

---
+++ WeatherApp/Services/WindowService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Interfaces;
+
+namespace WeatherApp.Services
+{
+	public class WindowService : IWindowService
+	{
+		public bool? ShowDialog(object viewModel)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ShowWindow(object viewModel)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
