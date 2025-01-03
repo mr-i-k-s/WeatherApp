bc503e39f58fd282278b717362db1b56e5eec4e2
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-03 10:24:32 +0800

    Add class WeatherService and Implement IWeatherService

---
+++ WeatherApp/Services/WeatherService.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using WeatherApp.Interfaces;
+using WeatherApp.MVVM.Models;
+
+namespace WeatherApp.Services
+{
+	public class WeatherService : IWeatherService
+	{
+		public string BaseWeatherApiUrl { get; } = "https://api.openweathermap.org/data/2.5/weather";
+
+		public Task<IWeatherData> GetWeatherDataAsync(string city, string state, string country, string unit, string apiKey)
+		{
+			IWeatherData weatherData = null;
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(BaseWeatherApiUrl);
+
+                var response = httpClient.GetAsync($"?q={city ?? ""},{state ?? ""},{country ?? ""}&appid={apiKey}&units={unit}").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = response.Content.ReadAsStringAsync().Result;
+                    weatherData = JsonConvert.DeserializeObject<WeatherData?>(jsonResponse);
+                }
+            }
+
+			return Task.FromResult(weatherData);
+		}
+	}
+}
