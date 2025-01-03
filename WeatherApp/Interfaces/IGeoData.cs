fc545e339a878806876947023018152da6d19e60
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-03 10:09:42 +0800

    Add Interface IGeoData

---
+++ WeatherApp/Interfaces/IGeoData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Interfaces
+{
+	public interface IGeoData
+	{
+		public string Name { get; set; }
+		public string Country { get; set; }
+		public string State { get; set; }
+	}
+}
