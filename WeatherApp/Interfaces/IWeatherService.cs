11b57b97b73b3896cabfaa014d23cd745e8dddc3
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-03 10:23:45 +0800

    Add Interface IWeatherService

---
+++ WeatherApp/Interfaces/IWeatherService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Interfaces
+{
+	public interface IWeatherService
+	{
+		Task<IWeatherData> GetWeatherDataAsync(string city, string state, string country, string unit, string apiKey);
+	}
+}
