d0ff36cfc5427130bda39ef6f1e75d1c00637fd7
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-03 09:54:51 +0800

    Add Interface IWindowService

---
+++ WeatherApp/Interfaces/IWindowService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Interfaces
+{
+	public interface IWindowService
+	{
+		void ShowWindow(object viewModel);
+		bool? ShowDialog(object viewModel);
+	}
+}
