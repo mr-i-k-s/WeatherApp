a3a224c602c70af30a6ef5479a6110e7f243ec7c
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-03 10:22:10 +0800

    Add Interface IGeoService

---
+++ WeatherApp/Interfaces/IGeoService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Interfaces
+{
+	public interface IGeoService
+	{
+		Task<IGeoData[]> GetGeoDataAsync(string city, string apiKey, int limit = 5);
+	}
+}
