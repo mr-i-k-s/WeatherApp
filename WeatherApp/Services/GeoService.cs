97dc9afd46934b1a0805df2bec42b774a277a3f6
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-03 10:23:05 +0800

    Add class GeoService and Implement IGeoService

---
+++ WeatherApp/Services/GeoService.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using WeatherApp.Interfaces;
+using WeatherApp.MVVM.Models;
+
+namespace WeatherApp.Services
+{
+	public class GeoService : IGeoService
+	{
+		public string BaseGeoApiUrl { get; } = "https://api.openweathermap.org/geo/1.0/direct";
+
+		public Task<IGeoData[]> GetGeoDataAsync(string city, string apiKey, int limit = 10)
+		{
+			IGeoData[] geoData = null;
+
+			using (var client = new HttpClient())
+			{
+				client.BaseAddress = new Uri(BaseGeoApiUrl);
+
+				var response = client.GetAsync($"?q={city}&limit={limit}&appid={apiKey}").Result;
+
+				if (response.IsSuccessStatusCode)
+				{
+					var jsonResponse = response.Content.ReadAsStringAsync().Result;
+					geoData = JsonConvert.DeserializeObject<GeoData[]?>(jsonResponse);
+				}
+			}
+
+			return Task.FromResult(geoData);
+		}
+	}
+}
